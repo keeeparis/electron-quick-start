43a877b75a1fbcfcaac4f969ee1079fdaac44a95
Keeeparis <vladimir1trotsenko@gmail.com>
2021-09-07 16:25:13 +0600

    Initial commit

--- renderer.js
+++ renderer.js
@@ -1,6 +1,18 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+import {TOKEN} from './modules/token.js'
+import createSelector from './modules/createSelector.js'
+import createData from './modules/createData.js'
+
+window.addEventListener('DOMContentLoaded', async () => { 
+    const res = await fetch(`http://data.fixer.io/api/latest?access_key=${TOKEN}`)
+    const latestCurrency = await res.json()
+    const res1 = await fetch(`http://data.fixer.io/api/symbols?access_key=${TOKEN}`)
+    const countrySymbols = await res1.json()
+    const res2 = await fetch('http://country.io/currency.json')
+    const country2currency = await res2.json()
+    const res3 = await fetch('https://flagcdn.com/en/codes.json')
+    const countryFullName = await res3.json()
+    
+    createSelector(countrySymbols)
+    createData(latestCurrency, country2currency, countryFullName)
+})
+
